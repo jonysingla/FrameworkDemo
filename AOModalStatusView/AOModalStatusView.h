cfc4006cb4df23331db8283ad135b68988637ca4
Jony <jony.singla@mind-infotech.com>
2019-07-04 10:49:48 +0530

    Initial Commit

---
+++ AOModalStatusView/AOModalStatusView.h
@@ -0,0 +1,19 @@
+//
+//  AOModalStatusView.h
+//  AOModalStatusView
+//
+//  Created by Jony on 04/07/19.
+//  Copyright © 2019 Jony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AOModalStatusView.
+FOUNDATION_EXPORT double AOModalStatusViewVersionNumber;
+
+//! Project version string for AOModalStatusView.
+FOUNDATION_EXPORT const unsigned char AOModalStatusViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AOModalStatusView/PublicHeader.h>
+
+
