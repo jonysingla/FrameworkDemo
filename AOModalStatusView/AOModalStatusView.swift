360831a9469fa3ea08b542116ede364252d2fcbf
jonysingla <jony.singla19@gmail.com>
2019-07-08 11:31:19 +0530

    first commit

---
+++ AOModalStatusView/AOModalStatusView.swift
@@ -0,0 +1,21 @@
+//
+//  AOModalStatusView.swift
+//  AOModalStatusView
+//
+//  Created by Jony on 04/07/19.
+//  Copyright © 2019 Jony. All rights reserved.
+//
+
+import UIKit
+
+class AOModalStatusView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
